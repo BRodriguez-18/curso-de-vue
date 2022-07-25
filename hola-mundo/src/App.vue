1eaa15fc73df89c1643c471dc79edf7a2f92934a
Brian Rodriguez <brianxd90@gmail.com>
2022-07-25 12:11:20 -0600

    probando vue cli

---
+++ hola-mundo/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld v-bind:msg="mensaje"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data(){
+    return{
+      mensaje: 'hola brian 33'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #f00b63;
+  margin-top: 60px;
+}
+</style>
